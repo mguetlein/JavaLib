ce2825cc3da6ae93f2e30dcfe80aa0efc5ffdc2f
Martin Gütlein <guetlein@posteo.de>
2016-01-05 11:38:08 +0100

    add compressed key value file store

--- src/main/java/org/mg/javalib/io/KeyValueFileStore.java
+++ src/main/java/org/mg/javalib/io/KeyValueFileStore.java
@@ -9,6 +9,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SerializationException;
 import org.apache.commons.lang3.SerializationUtils;
 import org.mg.javalib.util.ArrayUtil;
 
@@ -16,18 +18,24 @@ public class KeyValueFileStore<K, V extends Serializable>
 {
 	String dir;
 	Set<String> files;
+	boolean md5Enabled = false;
+	boolean compress = false;
 
-	public KeyValueFileStore(String dir)
+	public KeyValueFileStore(String dir, boolean md5Enabled, boolean compress)
 	{
 		if (!new File(dir).isDirectory())
 			throw new IllegalArgumentException("directory not found: " + dir);
 		this.dir = dir;
+		this.compress = compress;
 		files = new HashSet<>(ArrayUtil.toList(new File(dir).list()));
 	}
 
 	private String filename(K k)
 	{
-		return DigestUtils.md5Hex(k.toString());
+		if (md5Enabled)
+			return DigestUtils.md5Hex(k.toString());
+		else
+			return k.toString();
 	}
 
 	public synchronized boolean contains(K k)
@@ -38,7 +46,7 @@ public class KeyValueFileStore<K, V extends Serializable>
 		// dynamic update
 		if (new File(dir, name).exists())
 		{
-			files.add(dir);
+			files.add(name);
 			return true;
 		}
 		return false;
@@ -46,29 +54,53 @@ public class KeyValueFileStore<K, V extends Serializable>
 
 	public synchronized V get(K k)
 	{
+		FileInputStream i = null;
 		try
 		{
-			return SerializationUtils.deserialize(new FileInputStream(new File(dir, filename(k))));
+			i = new FileInputStream(new File(dir, filename(k)));
+			if (compress)
+				return SerializationUtilsCompressed.deserialize(i);
+			else
+				return SerializationUtils.deserialize(i);
+		}
+		catch (SerializationException ex)
+		{
+			System.err.println(filename(k));
+			throw ex;
 		}
 		catch (FileNotFoundException e)
 		{
 			throw new RuntimeException(e);
 		}
+		finally
+		{
+			if (i != null)
+				IOUtils.closeQuietly(i);
+		}
 	}
 
 	public synchronized void store(K k, V v)
 	{
+		FileOutputStream o = null;
 		try
 		{
 			String name = filename(k);
-			//			if (files.contains(name))
-			//				throw new IllegalArgumentException();
-			SerializationUtils.serialize(v, new FileOutputStream(new File(dir, name)));
+			new File(dir, name).getParentFile().mkdirs();
+			o = new FileOutputStream(new File(dir, name));
+			if (compress)
+				SerializationUtilsCompressed.serialize(v, o);
+			else
+				SerializationUtils.serialize(v, o);
 			files.add(name);
 		}
 		catch (Exception e)
 		{
 			throw new RuntimeException(e);
+		}
+		finally
+		{
+			if (o != null)
+				IOUtils.closeQuietly(o);
 		}
 	}
 
