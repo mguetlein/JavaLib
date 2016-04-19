86c0c17d3fb0af80f3134d80e448967a31da6912
Martin Gütlein <guetlein@posteo.de>
2016-04-19 19:01:16 +0200

    add flag to reload bundle properties

--- src/main/java/org/mg/javalib/util/ResourceBundleOwner.java
+++ src/main/java/org/mg/javalib/util/ResourceBundleOwner.java
@@ -7,20 +7,34 @@ public class ResourceBundleOwner
 {
 	private String resourceBundleName;
 
+	private boolean reload;
+
 	private ResourceBundle bundle;
 
 	public ResourceBundleOwner(String resourceBundleName)
 	{
+		this(resourceBundleName, false);
+	}
+
+	public ResourceBundleOwner(String resourceBundleName, boolean reload)
+	{
 		this.resourceBundleName = resourceBundleName;
+		this.reload = reload;
 	}
 
 	private ResourceBundle bundle()
 	{
-		if (bundle == null)
-			bundle = ResourceBundle.getBundle(resourceBundleName);
-		//		ResourceBundle.clearCache();
-		//		bundle = ResourceBundle.getBundle(resourceBundleName);
-		return bundle;
+		if (reload)
+		{
+			ResourceBundle.clearCache();
+			return ResourceBundle.getBundle(resourceBundleName);
+		}
+		else
+		{
+			if (bundle == null)
+				bundle = ResourceBundle.getBundle(resourceBundleName);
+			return bundle;
+		}
 	}
 
 	public String text(String key)
